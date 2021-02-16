64743b8caaccd12d39707c9c966d4fcbcc3e9b0a
JonasDeBoeck <Jonas.deboeck@student.ucll.be>
2021-02-16 15:04:34 +0100

    WidgetRest

--- src/main/java/hhh/acs/rest/WidgetREST.java
+++ src/main/java/hhh/acs/rest/WidgetREST.java
@@ -2,9 +2,7 @@ package hhh.acs.rest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import hhh.acs.database.DatabaseException;
-import hhh.acs.database.DoorRepository;
-import hhh.acs.database.WidgetRepository;
+import hhh.acs.database.*;
 import hhh.acs.model.Door;
 import hhh.acs.model.Widget;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.validation.Valid;
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ScheduledExecutorService;
@@ -21,63 +20,57 @@ import java.util.concurrent.ScheduledExecutorService;
 public class WidgetREST {
 
     @Autowired
-    public WidgetRepository widgetRepository;
+    public WidgetService widgetService;
     @Autowired
-    public DoorRepository doorRepository;
+    public DoorService doorService;
 
     @CrossOrigin()
     @GetMapping("/widgets")
     public List<Widget> getAll(){
-        return widgetRepository.findAll();
+        return widgetService.getAll();
     }
 
     @CrossOrigin()
     @GetMapping("/widgets/{id}")
     public Widget get(@PathVariable("id") int id){
-        return widgetRepository.findById(id).orElseThrow(() -> new DatabaseException("Widget with " + id + " not found in the database"));
+        try {
+            return widgetService.get(id);
+        } catch (DatabaseException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     @CrossOrigin()
     @PostMapping("/create")
     public Widget create(@RequestBody Widget widget){
-        List<Door> persistedDoors = new ArrayList<>();
-        for (Door door : widget.getDoors()){
-            Door d = doorRepository.findById(door.getId()).orElseGet(() -> null);
-            if (d == null){
-                persistedDoors.add(doorRepository.save(door));
-            }
-            else{
-                persistedDoors.add(d);
-            }
+        try {
+            return widgetService.create(widget);
+        } catch (DatabaseException e) {
+            System.out.println(e.getMessage());
+            return null;
         }
-        widget.setDoors(persistedDoors);
-        return widgetRepository.save(widget);
     }
 
     @CrossOrigin()
     @DeleteMapping("/delete/{id}")
     public Widget deleteWidget(@PathVariable("id") int id){
-        System.out.println("deleting widget with id " + id);
-        Widget widget = widgetRepository.findById(id).orElseThrow(() -> new DatabaseException("Widget with " + id + " not found in the database"));
-        widgetRepository.delete(widget);
-        return widget;
+        try {
+            return widgetService.delete(id);
+        } catch (DatabaseException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     @CrossOrigin()
     @PutMapping("/update/{id}")
     public String updateWidget(@PathVariable("id") int id, @RequestBody Widget widget){
-        System.out.println(id);
-        List<Door> newDoors = widget.getDoors();
-        Widget oldWidget = widgetRepository.findById(id).orElseThrow(() -> new DatabaseException("Widget with " + id + " not found in the database"));
-        List<Door> oldDoors = oldWidget.getDoors();
-        if (newDoors != null){
-            for (Door door : oldDoors){
-                if (!newDoors.contains(door)){
-                    doorRepository.delete(door);
-                }
-            }
+        try {
+            return widgetService.updateWidget(id, widget);
+        } catch (DatabaseException e) {
+            System.out.println(e.getMessage());
+            return null;
         }
-        widgetRepository.save(widget);
-        return widget.toString();
     }
 }
