047187bbef12bbbce952471d979404952f992cae
RafaelBackx <rafaelbackx@live.be>
2021-03-02 13:39:08 +0100

    corsfilter toegevoegd

---
+++ src/main/java/hhh/acs/configuration/CorsFilter.java
@@ -0,0 +1,32 @@
+package hhh.acs.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsFilter implements Filter {
+    private final Logger log = LoggerFactory.getLogger(CorsFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
+        response.setHeader("Access-Control-Allow-Origin","*");
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
